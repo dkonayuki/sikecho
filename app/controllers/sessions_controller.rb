71ce88f9bb4f1e8404ed4544e1959dc2d07ffb3f
dkonayuki <dkonayuki@gmail.com>
2014-01-13 02:19:23 +0900

    created Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_name(params[:name]);
+    if user and user.authenticate(params[:password]);
+      session[:user_id] = user.id
+      redirect_to home_path
+    else 
+      redirect_to login_url, alert: "Wrong password or username!"
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Shikechou::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   get "home/index"
   resources :users
 
