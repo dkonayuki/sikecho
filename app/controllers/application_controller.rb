277a2540da6de2915006d224bbb883cfcb56b68c
dkonayuki <dkonayuki@gmail.com>
2014-01-17 01:31:41 +0900

    add authorize before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  private
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, notice: "Please login"
+    end
+  end
 end
