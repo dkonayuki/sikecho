db9b5ab3baa42bdb1a94e91a1862ca4aff036d4c
dkonayuki <dkonayuki@gmail.com>
2014-02-03 05:19:44 +0900

    add new subject page

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -13,6 +13,14 @@ class ScheduleController < ApplicationController
   end
 
   def new
+    @period = Period.new
+    @subjects = Subject.all
+    time_names = %w(一時限 二時限 三時限 四時限 五時限 六時限)
+    day_names = %w(月曜日 火曜日 水曜日 木曜日 金曜日 土曜日)
+    @period.time = params[:time].to_i
+    @period.time_name = time_names[ params[:time].to_i ]
+    @period.day = params[:day].to_i
+    @period.day_name = day_names[ params[:day].to_i]
   end
   
   def create
