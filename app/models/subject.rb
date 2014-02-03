dac6ecb55c598dd2568df928ad57002ed7cd99bd
dkonayuki <dkonayuki@gmail.com>
2014-02-03 20:14:07 +0900

    implement search feature

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,12 @@ class Subject < ActiveRecord::Base
   has_and_belongs_to_many :faculties
   has_and_belongs_to_many :notes
   has_many :periods
+  
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
