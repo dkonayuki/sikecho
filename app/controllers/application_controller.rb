1c888a3d4b06f097f5de35b95703fb81ee52e284
dkonayuki <dkonayuki@gmail.com>
2014-02-21 16:26:02 +0900

    disable browser caching, remove back button in note show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :authorize, :current_user
+  before_filter :authorize, :current_user, :set_no_cache
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -22,6 +22,12 @@ class ApplicationController < ActionController::Base
   private
   def disable_nav
     @disable_nav = true
+  end
+  
+  def set_no_cache
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 
 end
