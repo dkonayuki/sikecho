069da4b047e19aeaae9d6653047003cb2f36a0d0
dkonayuki <dkonayuki@gmail.com>
2014-09-10 20:18:29 +0900

    add notification feature in subject show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap
 //= require bootstrap-sprockets
 //= require bootstrap-select.js
 //= require bootstrap-tagsinput.js
