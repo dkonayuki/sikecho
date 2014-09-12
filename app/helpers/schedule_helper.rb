3581700f964a771f98a71287a50e62f55f845ba8
dkonayuki <dkonayuki@gmail.com>
2014-09-12 22:19:06 +0900

    use proper way to store model specific constants #2

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -3,8 +3,8 @@ module ScheduleHelper
     #For schedule rendering
     @periods = Hash.new
     @user = current_user
-    1.upto(Period.MAX_DAY).each do | d | 
-      1.upto(Period.MAX_TIME).each do | t |
+    1.upto(Period::MAX_DAY).each do | d | 
+      1.upto(Period::MAX_TIME).each do | t |
         key = [d, t]
         @periods[key] = @user.current_subjects.joins(:periods).where(periods: {day: d, time: t}) || []
         # => @periods[key] = @user.subjects.joins(:periods).where(periods: {day: d, time: t})
