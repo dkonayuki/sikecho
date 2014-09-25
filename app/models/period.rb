9e80cff4153be763f85af5ded2ab382d9892cc9f
dkonayuki <dkonayuki@gmail.com>
2014-09-25 18:12:39 +0900

    add ja support for wysihtml5, improve mobile desgin, continue i18n(process----30%)

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,22 +1,6 @@
 class Period < ActiveRecord::Base
   belongs_to :subject
   
-  def self.day_names(day)
-    %w(全て 月曜日 火曜日 水曜日 木曜日 金曜日 土曜日)[day]
-  end
-  
-  def self.day_names_short(day)
-    %w(全 月 火 水 木 金 土)[day]
-  end
-  
-  def self.time_names(time)
-    %w(全て 一時限 二時限 三時限 四時限 五時限 六時限 七時限 八時限)[time]
-  end
-  
-  def self.time_names_short(time)
-    %w(全 1 2 3 4 5 6 7 8)[time]
-  end
-  
   MAX_TIME = 7
   MAX_DAY = 6
 
--- vendor/assets/javascripts/wysihtml5/index.js
+++ vendor/assets/javascripts/wysihtml5/index.js
@@ -2,4 +2,5 @@
 //= require ./templates.js
 //= require ./bootstrap3-wysihtml5.js
 //= require ./bootstrap-wysihtml5.en-US.js
+//= require ./bootstrap-wysihtml5.ja-JP.js
 //= require ./commands.js
