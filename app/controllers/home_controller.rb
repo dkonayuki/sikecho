456ff6d367acb243ea99dad2dd200cd3f717dcf1
dkonayuki <dkonayuki@gmail.com>
2014-10-23 12:30:59 +0900

    fix bug in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,12 @@
 class HomeController < ApplicationController
   def index
-    if current_university == nil
-      @universities = University.all
-    else
-      @university = current_university
-    end
-
     #for @user in view
     if user_signed_in? 
       @user = current_user
       @activities = PublicActivity::Activity.order('created_at desc')
         .where('(trackable_id in (?) AND trackable_type = ?) OR (trackable_id in (?) AND trackable_type = ?)', @user.current_subjects.ids, 'Subject', @user.registered_notes.ids, 'Note')
+    else
+      @universities = University.all
     end
   end
 end
