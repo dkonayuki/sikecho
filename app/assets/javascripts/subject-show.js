e1af0c94f71b5bd8be2b3236505e89ef6c71306f
dkonayuki <dkonayuki@gmail.com>
2015-01-11 17:25:15 +0900

    add confirm message when register/remove subjects

--- app/assets/javascripts/subject-show.js
+++ app/assets/javascripts/subject-show.js
@@ -38,51 +38,60 @@ $(document).on("page:load ready", function() {
 		/*For schedule add btn in show subject page*/
 		/*Need selector on a to ensure binding on dynamically elements */
 		$("#show-subject-schedule").add("#show-subject-schedule-mobile").on("click", "a", function() {
+
 			var href = $(this).attr("href");
 			var dataMethod = $(this).attr("data-method");
 			var subjectName = $("#show-subject-schedule").data("name");
 			var subjectID = $("#show-subject-schedule").data("id");
 			var imgPath = $("#show-subject-schedule").data("img");
 			
-			$.ajax({
-				url: href,
-				type: dataMethod,
-				dataType: "json",
-				success: function(msg) {
-					if (msg.status == "ok") {
-						//need to get script in order to refresh menu
-						$.getScript("/" + I18n["meta"]["code"] + "/subjects/" + subjectID, null);
-	
-						//Set notification
-						if (dataMethod == "post") {
-							$.gritter.add({
-								title: subjectName,
-								text: I18n["subject_register_notification"],
-								image: imgPath,
-								sticky: false, 
-								time: 5000,
-							});
+			if (dataMethod == "post") {
+				var r = confirm(I18n["subject_register_confirm"]);
+			} else {
+				var r = confirm(I18n["subject_remove_confirm"]);
+			}
+			
+			if (r == true) {				
+				$.ajax({
+					url: href,
+					type: dataMethod,
+					dataType: "json",
+					success: function(msg) {
+						if (msg.status == "ok") {
+							//need to get script in order to refresh menu
+							$.getScript("/" + I18n["meta"]["code"] + "/subjects/" + subjectID, null);
+		
+							//Set notification
+							if (dataMethod == "post") {
+								$.gritter.add({
+									title: subjectName,
+									text: I18n["subject_register_notification"],
+									image: imgPath,
+									sticky: false, 
+									time: 5000,
+								});
+							} else {
+								$.gritter.add({
+									title: subjectName,
+									text: I18n["subject_remove_notification"],
+									image: imgPath,
+									sticky: false, 
+									time: 5000,
+								});
+							}
 						} else {
 							$.gritter.add({
 								title: subjectName,
-								text: I18n["subject_remove_notification"],
+								text: I18n["error"],
 								image: imgPath,
 								sticky: false, 
 								time: 5000,
 							});
 						}
-					} else {
-						$.gritter.add({
-							title: subjectName,
-							text: I18n["error"],
-							image: imgPath,
-							sticky: false, 
-							time: 5000,
-						});
+	
 					}
-
-				}
-			});
+				});
+			}
 			return false;
 		});
 		
