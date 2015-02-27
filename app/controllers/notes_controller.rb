398d275d3e283e4f0881174eb61a6d23faa63d30
dkonayuki <dkonayuki@gmail.com>
2015-02-28 02:22:58 +0900

    change number of notes per 1 page back to 20

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -69,7 +69,7 @@ class NotesController < ApplicationController
     @notes = @notes.search(params[:search])
     
     #paginate @notes
-    @notes = @notes.page(params[:page]).per(4)
+    @notes = @notes.page(params[:page]).per(20)
     
     case @user.settings(:note).layout
     when :all
